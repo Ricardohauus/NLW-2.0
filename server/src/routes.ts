43bedf8dddbbbbf6024dbf1f0d580e1bc00b8c65
Ricardo Brasil <ricardohauus@gmail.com>
2020-08-06 23:56:42 -0300

    inserido a rota de connections

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 export const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 routes.post('/classes', classesControllers.create);
 routes.get('/classes', classesControllers.index);
 
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
