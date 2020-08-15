612838e430efe3e86dd5db76b34e0be10d947d60
Ricardo Brasil <ricardohauus@gmail.com>
2020-08-15 03:32:37 -0300

    refatorando a tela de TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,19 +1,53 @@
 import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
-
-import PageHeader from '../../components/PageHeader';
-
-import styles from './styles';
-import TeacherItem from '../../components/TeacherItem';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons'
+import AsynctStorage from '@react-native-community/async-storage'
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import api from '../../services/api';
+import styles from './styles';
+import { useFocusEffect } from '@react-navigation/native';
+
 function TeacherList() {
 
   const [isFiltersVisible, setIsFilterVisible] = useState(false);
+  const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
 
+  const [subject, setSubject] = useState('Matematica');
+  const [week_day, setWeekDay] = useState('1');
+  const [time, setTime] = useState('8:00');
+
+  function loadFavorites() {
+    AsynctStorage.getItem('favorites').then(res => {
+      if (res) {
+        const favoritedTeachers = JSON.parse(res);
+        const favoritedTeachersIds = favoritedTeachers.map((t: Teacher) => {
+          return t.id
+        })
+        setFavorites(favoritedTeachersIds);
+      }
+    })
+  }
   function handleToggleFiltersVisible() {
     setIsFilterVisible(!isFiltersVisible);
   }
+
+  async function handleFiltersSubmit() {
+    loadFavorites();
+    const res = await api.get('classes', {
+      params: { subject, week_day, time }
+    })
+    setIsFilterVisible(false);
+    setTeachers(res.data);
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  })
 
   return (
     <View style={styles.container}>
@@ -28,6 +62,8 @@ function TeacherList() {
             <Text style={styles.label}>Matéria</Text>
             <TextInput
               style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
               placeholder="Qual a matéria?"
               placeholderTextColor="#c1bccc"
             />
@@ -36,6 +72,8 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
                   style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
                   placeholder="Qual o Dia?"
                   placeholderTextColor="#c1bccc"
                 />
@@ -44,15 +82,16 @@ function TeacherList() {
                 <Text style={styles.label}>Horário</Text>
                 <TextInput
                   style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
                   placeholder="Qual o horario?"
                   placeholderTextColor="#c1bccc"
                 />
               </View>
             </View>
 
-            <RectButton style={styles.submitButton}>
+            <RectButton style={styles.submitButton} onPress={handleFiltersSubmit} >
               <Text style={styles.submitButtonText}> Filtrar </Text>
-
             </RectButton>
 
           </View>
@@ -62,10 +101,17 @@ function TeacherList() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+
+            />)
+        })}
+
+
       </ScrollView>
     </View>
   );
