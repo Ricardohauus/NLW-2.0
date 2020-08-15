661541b6ee84b3448e0fd418bdd63ce52d715a51
Ricardo Brasil <ricardohauus@gmail.com>
2020-08-15 03:32:02 -0300

    refatorando a tela de favoritos

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
-
+import AsynctStorage from '@react-native-community/async-storage';
 import PageHeader from '../../components/PageHeader';
+import { useFocusEffect } from '@react-navigation/native';
 
 import styles from './styles';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsynctStorage.getItem('favorites').then(res => {
+      if (res) {
+        const favoritedTeachers = JSON.parse(res);
+        setFavorites(favoritedTeachers);
+      }
+    })
+  }
+  useFocusEffect(() => {
+    loadFavorites();
+  });
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
@@ -14,10 +28,15 @@ function Favorites() {
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={true}
+
+            />)
+        })}
       </ScrollView>
     </View>
   );
